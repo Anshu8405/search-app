4912027b2fb1473520ea1f6d7e4513bda5cd9b15
Brajesh Yadav <Brajesh.Yadav@Chubb.com>
2021-05-06 22:50:58 +0800

    samp;e test added

---
+++ src/components/Search.test.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Enzyme, { shallow, mount } from 'enzyme';
+import { Search } from "./Search";
+import { Adapter } from "enzyme-adapter-react-16";
+
+Enzyme.configure({adapter = new  Adapter()});
+
+describe("Search Component",() => {
+    test("render", () => {
+        const wrapper = shallow(<Search />);
+        expect(wrapper.exists()).toBe(true);
+    });
+});
